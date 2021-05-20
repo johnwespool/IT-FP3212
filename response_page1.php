b781ffdf62ecf88ddad7531d88191660532f4215
johnwespool <80968598+johnwespool@users.noreply.github.com>
2021-05-20 19:17:19 -0400

    Add files via upload

---
+++ response_page1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["fname"]; ?><br>
+<?php echo $_GET["lname"]; ?><br>
+<?php echo $_GET["email"]; ?><br>
+<?php echo $_GET["student_status"]; ?><br>
+<?php echo $_GET["optin"]; ?><br>
+<?php echo $_GET["comments"]; ?><br>
+
+
+</body>
+</html>
